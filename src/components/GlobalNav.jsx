79e2f5c293ed02edf8af9e1b55fe973c1d3eaf3f
Chaldrak <chaldrakus@gmail.com>
2023-01-17 17:09:31 +0100

    GlobalNav added

--- src/components/GlobalNav.jsx
+++ src/components/GlobalNav.jsx
@@ -1,8 +1,34 @@
 import React from "react";
 import useAuth from "../hooks/useAuth";
-import { Button } from "@material-tailwind/react";
 import { removeItem } from "../services/localStorage";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  Navbar,
+  MobileNav,
+  Typography,
+  Button,
+  IconButton,
+} from "@material-tailwind/react";
+
+const menu = [
+  {
+    name: "Pages",
+    path: "/pages",
+  },
+  {
+    name: "Account",
+    path: "/account",
+  },
+  {
+    name: "FAQ",
+    path: "/faq",
+  },
+  {
+    name: "Leaderboard",
+    path: "/leaderboard",
+  },
+];
 
 const GlobalNav = () => {
   const navigate = useNavigate();
@@ -12,14 +38,101 @@ const GlobalNav = () => {
     navigate("/signin");
     setAuth({});
   };
+  const [openNav, setOpenNav] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false)
+    );
+  }, []);
+
+  const navList = (
+    <ul className="mb-4 mt-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      {menu.map((item, index) => (
+        <Typography
+          as="li"
+          variant="medium"
+          color="blue-gray"
+          className="p-1 font-bold hover:text-yellow-900"
+          key={index}
+        >
+          <NavLink to={item.path} className="flex items-center">
+            {item.name}
+          </NavLink>
+        </Typography>
+      ))}
+    </ul>
+  );
   if (auth?.token)
     return (
-      <div className="flex items-center justify-between p-2">
-        <p>GlobalNav</p>
-        <Button color="red" size="sm" onClick={logout}>
-          Logout
-        </Button>
-      </div>
+      <Navbar className="max-w-screen-7xl mx-auto w-full rounded-none py-2 px-4 lg:px-8">
+        <div className="container mx-auto flex items-center justify-between text-blue-gray-900">
+          <Typography
+            variant="small"
+            className="mr-4 cursor-pointer py-1.5 font-normal"
+          >
+            <NavLink to="/">
+              <img
+                src="https://res.cloudinary.com/dumxkdcvd/image/upload/v1673437258/9f008cbb44537ec3910105d779c54b49_soswu2.png"
+                alt="logo manga quizz"
+                className="h-7"
+              />
+            </NavLink>
+          </Typography>
+          <div className="hidden lg:block">{navList}</div>
+          <Button
+            variant="gradient"
+            size="sm"
+            className="hidden lg:inline-block"
+          >
+            <span>Buy Now</span>
+          </Button>
+          <IconButton
+            variant="text"
+            className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+            ripple={false}
+            onClick={() => setOpenNav(!openNav)}
+          >
+            {openNav ? (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                className="h-6 w-6"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            ) : (
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16M4 18h16"
+                />
+              </svg>
+            )}
+          </IconButton>
+        </div>
+        <MobileNav open={openNav}>
+          {navList}
+          <Button variant="gradient" size="sm" fullWidth className="mb-2">
+            <span>Buy Now</span>
+          </Button>
+        </MobileNav>
+      </Navbar>
     );
 };
 
